ddf67f85a3d3ec293899ade2a6eb303570fb4c39
Frikker <stolpnikk.97@gmail.com>
2020-12-08 23:24:52 +0300

    started to learn Angular

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const compression = require('compression');
 
 const app = express();
- 
-app.use(compression())
+
+app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'dist/todolist-frontend')));
 
@@ -17,4 +17,4 @@ const port = process.env.PORT || 3000;
 app.set('port', port)
 
 const server = http.createServer(app)
-server.listen(port, () => console.log('server started ${ port }'))
+server.listen(port, () => console.log("server started"))
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
